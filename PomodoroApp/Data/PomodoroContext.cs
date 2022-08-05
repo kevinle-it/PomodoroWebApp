216784c2ca7757fa1df7cb6cc1a6d1655b85ebd2
Tri Le <trile95.it@gmail.com>
2022-08-04 20:55:22 -0400

    add pomodoro db context

---
+++ PomodoroApp/Data/PomodoroContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace PomodoroApp.Data
+{
+    public class PomodoroContext : DbContext
+    {
+        public PomodoroContext(DbContextOptions<PomodoroContext> options) : base(options)
+        {
+        }
+    }
+}
+
