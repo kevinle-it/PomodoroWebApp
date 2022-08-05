0bd6c0a03ec12eb060c1b3e660ff882729059ed4
Tri Le <trile95.it@gmail.com>
2022-08-04 23:27:16 -0400

    add some integration tests for pomodoro api controller

---
+++ PomodoroTest/IntegrationTestPomodoroApiController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PomodoroApp.Controllers;
+using PomodoroApp.Data;
+using PomodoroTask = PomodoroApp.Data.Entities.Task;
+
+namespace PomodoroTest
+{
+    [TestClass]
+    public class IntegrationTestPomodoroApiController
+    {
+        [TestMethod]
+        public void PomodoroApiControllerCanReturnAllTasks()
+        {
+            DbContextOptionsBuilder<PomodoroContext> optionsBuilder = new();
+            optionsBuilder.UseInMemoryDatabase(MethodBase.GetCurrentMethod().Name);
+
+            var dateTimeCreatedTask = new DateTime(2022, 8, 4, 22, 59, 59, DateTimeKind.Utc);
+            using (PomodoroContext ctx = new(optionsBuilder.Options))
+            {
+                ctx.Add(new PomodoroTask {
+                    Name = "Complete Web Final Project",
+                    NumEstimatedPoms = 4,
+                    NumCompletedPoms = 4,
+                    NumCompletedShortBreaks = 3,
+                    IsCompletedLongBreak = true,
+                    DateTimeCreated = dateTimeCreatedTask
+                });
+                ctx.SaveChanges();
+            }
+
+            Task<ActionResult<IEnumerable<PomodoroTask>>> result;
+            using (PomodoroContext ctx = new(optionsBuilder.Options))
+            {
+                result = new PomodoroController(ctx).GetPomodoroTasks();
+            }
+
+            List<PomodoroTask> pomodoroTasks = (List<PomodoroTask>) result.Result.Value;
+            Assert.IsNotNull(pomodoroTasks);
+            Assert.AreEqual(1, pomodoroTasks.Count);
+
+            var firstPomodoroTask = pomodoroTasks.First();
+            Assert.AreEqual(1, firstPomodoroTask.TaskId);
+            Assert.AreEqual("Complete Web Final Project", firstPomodoroTask.Name);
+            Assert.AreEqual(4, firstPomodoroTask.NumEstimatedPoms);
+            Assert.AreEqual(4, firstPomodoroTask.NumCompletedPoms);
+            Assert.AreEqual(3, firstPomodoroTask.NumCompletedShortBreaks);
+            Assert.AreEqual(true, firstPomodoroTask.IsCompletedLongBreak);
+            Assert.AreEqual(dateTimeCreatedTask, firstPomodoroTask.DateTimeCreated);
+        }
+
+        [TestMethod]
+        public void PomodoroApiControllerCanProcessOnCurrentPomodoroComplete()
+        {
+            DbContextOptionsBuilder<PomodoroContext> optionsBuilder = new();
+            optionsBuilder.UseInMemoryDatabase(MethodBase.GetCurrentMethod().Name);
+
+            var dateTimeCreatedTask = new DateTime(2022, 8, 4, 22, 59, 59, DateTimeKind.Utc);
+            using (PomodoroContext ctx = new(optionsBuilder.Options))
+            {
+                ctx.Add(new PomodoroTask
+                {
+                    Name = "Complete Web Final Project",
+                    NumEstimatedPoms = 4,
+                    NumCompletedPoms = 3,
+                    NumCompletedShortBreaks = 3,
+                    IsCompletedLongBreak = false,
+                    DateTimeCreated = dateTimeCreatedTask
+                });
+                ctx.SaveChanges();
+            }
+
+            Task<ActionResult<PomodoroTask>> result;
+            using (PomodoroContext ctx = new(optionsBuilder.Options))
+            {
+                result = new PomodoroController(ctx).OnCurrentPomodoroComplete(1);
+            }
+
+            PomodoroTask pomodoroTask = (PomodoroTask) ((OkObjectResult) result.Result.Result).Value;
+            Assert.IsNotNull(pomodoroTask);
+
+            Assert.AreEqual(1, pomodoroTask.TaskId);
+            Assert.AreEqual("Complete Web Final Project", pomodoroTask.Name);
+            Assert.AreEqual(4, pomodoroTask.NumEstimatedPoms);
+            Assert.AreEqual(4, pomodoroTask.NumCompletedPoms);
+            Assert.AreEqual(3, pomodoroTask.NumCompletedShortBreaks);
+            Assert.AreEqual(false, pomodoroTask.IsCompletedLongBreak);
+            Assert.AreEqual(dateTimeCreatedTask, pomodoroTask.DateTimeCreated);
+
+            using (PomodoroContext ctx = new(optionsBuilder.Options))
+            {
+                PomodoroTask inDbPomodoroTask = ctx.Tasks.FindAsync(1).Result;
+
+                Assert.AreEqual(1, inDbPomodoroTask.TaskId);
+                Assert.AreEqual("Complete Web Final Project", inDbPomodoroTask.Name);
+                Assert.AreEqual(4, inDbPomodoroTask.NumEstimatedPoms);
+                Assert.AreEqual(4, inDbPomodoroTask.NumCompletedPoms);
+                Assert.AreEqual(3, inDbPomodoroTask.NumCompletedShortBreaks);
+                Assert.AreEqual(false, inDbPomodoroTask.IsCompletedLongBreak);
+                Assert.AreEqual(dateTimeCreatedTask, inDbPomodoroTask.DateTimeCreated);
+            }
+        }
+    }
+}
+
