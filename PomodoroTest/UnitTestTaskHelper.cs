4f104d613eaff6267f8608e6a774edc30d86f1b4
Tri Le <trile95.it@gmail.com>
2022-08-04 21:28:00 -0400

    add unit tests for task helper class

---
+++ PomodoroTest/UnitTestTaskHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PomodoroLogic;
+
+namespace PomodoroTest
+{
+    [TestClass]
+    public class UnitTestTaskHelper
+    {
+        TaskHelper taskHelper;
+
+        [TestInitialize]
+        public void Instantiate()
+        {
+            taskHelper = new TaskHelper();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            taskHelper = null;
+        }
+
+        [TestMethod]
+        public void TaskHelperClassIsInstantiable()
+        {
+            Assert.IsNotNull(taskHelper);
+        }
+
+        [TestMethod]
+        [DataRow(true, 2, 2, true)]
+        [DataRow(false, 6, 5, false)]
+        [DataRow(false, 9, 8, false)]
+        [DataRow(true, 15, 18, true)]
+        [DataRow(true, 20, 20, true)]
+        [DataRow(true, 22, 29, true)]
+        public void taskHelperCanDetermineIfTaskIsCompleted(bool expectedIsTaskCompleted, int numEstimatedPoms, int numCompletedPoms, bool isCompletedLongBreak)
+        {
+            Assert.AreEqual(expectedIsTaskCompleted, taskHelper.isTaskCompleted(numEstimatedPoms, numCompletedPoms, isCompletedLongBreak));
+        }
+    }
+}
+
