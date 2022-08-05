e8937ff04e5f2ad54a74fd61784bb4f8d34b0a79
Tri Le <trile95.it@gmail.com>
2022-08-04 22:32:08 -0400

    add initial db migration

---
+++ PomodoroApp/Migrations/Sqlite/20220805023136_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PomodoroApp.Migrations.Sqlite
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    NumEstimatedPoms = table.Column<int>(type: "INTEGER", nullable: false),
+                    NumCompletedPoms = table.Column<int>(type: "INTEGER", nullable: false),
+                    NumCompletedShortBreaks = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsCompletedLongBreak = table.Column<bool>(type: "INTEGER", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.TaskId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
