3733b1b9ef4039f0dfa99aee32f4f8cc8e277b6d
Tri Le <trile95.it@gmail.com>
2022-08-17 18:48:11 -0400

    hide unnecessary task id in home component and display message for each mode respectively

--- PomodoroApp/ClientApp/src/components/Home/index.js
+++ PomodoroApp/ClientApp/src/components/Home/index.js
@@ -1,29 +1,71 @@
 import { createSelector } from '@reduxjs/toolkit';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { selectCurrentTaskId, selectCurrentTaskName } from '../../store/selectors/pomodoroSelector';
+import { getCurrentPomodoroMode, POMODORO_MODES } from '../../helpers/pomodoroHelper';
+import {
+  selectConfigs,
+  selectCurrentTaskId,
+  selectCurrentTaskName,
+  selectNumCompletedPoms, selectNumCompletedShortBreaks,
+} from '../../store/selectors/pomodoroSelector';
 import CountdownTimer from '../CountdownTimer';
 import TaskManager from '../TaskManager';
 import './styles.scss';
 
 const Home = () => {
-  const { currentTaskId = 1, currentTaskName } = useSelector(
+  const {
+    currentTaskId = 1,
+    currentTaskName,
+    numCompletedPoms,
+    numCompletedShortBreaks,
+    configs = {},
+  } = useSelector(
     createSelector(
       selectCurrentTaskId,
       selectCurrentTaskName,
-      (currentTaskId, currentTaskName) => ({
+      selectNumCompletedPoms,
+      selectNumCompletedShortBreaks,
+      selectConfigs,
+      (
         currentTaskId,
         currentTaskName,
+        numCompletedPoms,
+        numCompletedShortBreaks,
+        configs,
+      ) => ({
+        currentTaskId,
+        currentTaskName,
+        numCompletedPoms,
+        numCompletedShortBreaks,
+        configs,
       }),
     ));
+
+  const currentMode = useMemo(() => getCurrentPomodoroMode(
+    numCompletedPoms,
+    numCompletedShortBreaks,
+    configs.longBreakInterval,
+  ), [configs, numCompletedPoms, numCompletedShortBreaks]);
+
+  const modeMessage = useMemo(() => {
+    switch (currentMode) {
+      case POMODORO_MODES.POMODORO.type:
+        return 'Time to focus!';
+      case POMODORO_MODES.SHORT_BREAK.type:
+      case POMODORO_MODES.LONG_BREAK.type:
+        return 'Time for a break!';
+      default:
+        return 'Time to focus!';
+    }
+  }, [currentMode]);
 
   return (
     <div className="home__wrapper">
       <CountdownTimer />
       <div style={{ marginBottom: 10 }} />
-      <div className="home__task-id">#{currentTaskId}</div>
+      {/*<div className="home__task-id">#{currentTaskId}</div>*/}
       <div className="home__current-task-name">
-        {currentTaskName || 'Time to focus!'}
+        {currentTaskName || modeMessage}
       </div>
       <div style={{ marginBottom: 15 }} />
       <TaskManager />
